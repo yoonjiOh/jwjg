e3fbc8884292e71b394f84659315270fb308bfef
yoonjiO <yoonji@dable.io>
2020-12-21 01:33:30 +0900

    test

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 cd ./build
 
-echo 1
 npm cache clean --force
-echo 2
-rm -rf node_modules/
-echo 3
+rm -rf node_modules package-lock.json
 
 npm install
-echo 4
+
 npm run build
-echo 5
 npm run start
-echo 6
