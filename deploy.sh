299f8dc95f24dbf22597cb4ba1710cab4dcb2917
yoonjiO <yoonji@dable.io>
2020-12-21 01:26:15 +0900

    test

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 cd ./build
 
+echo 1
 npm cache clean --force
+echo 2
 rm -rf node_modules/
+echo 3
 
-npm install -gs
+npm install
+echo 4
 npm run build
+echo 5
 npm run start
+echo 6s
