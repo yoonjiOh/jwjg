12280b3f538915ca7751c56e18fa11abfa40d616
Juram Park <parkjuram@gmail.com>
2021-01-31 13:03:26 +0900

    implements user signup.

--- pages/api/resolvers/Mutation.js
+++ pages/api/resolvers/Mutation.js
@@ -1,35 +1,36 @@
-
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 async function createIssue(parent, args, context) {
-	const newIssue = await context.prisma.issue.create({
-		data: {
-			title: args.title,
-			content: args.content,
-			tag_id: args.tag_id
-		}
-	})
+  const newIssue = await context.prisma.issue.create({
+    data: {
+      title: args.title,
+      content: args.content,
+      tag_id: args.tag_id,
+    },
+  });
 
-	return newIssue;
+  return newIssue;
 }
 
-const APP_SECRET = 'jwjg-best-luck34';
+const APP_SECRET = "jwjg-best-luck34";
 
 // For type:user
 // TODO(jurampark): accepts firebase id token to support social auth like google/apple login.
 async function signup(parent, args, context, info) {
-	const password = await bcrypt.hash(args.password, 10)
-	const user = await context.prisma.user.create({ data: { ...args, password } })
-	const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  const password = await bcrypt.hash(args.password, 10);
+  const user = await context.prisma.user.create({
+    data: { ...args, password },
+  });
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
 
-	return {
-	  token,
-	  user,
-	}
-  }
+  return {
+    token,
+    user,
+  };
+}
 
 module.exports = {
-	createIssue,
-	signup
-}
+  createIssue,
+  signup,
+};
