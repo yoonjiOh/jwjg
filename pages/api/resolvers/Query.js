51528ad44ff868e2434dc91311f37157a74780b8
Jeesoo Hong <jeesoo@dable.io>
2021-02-14 00:22:26 +0900

    issue, post, user, tag 관련 쿼리 타입 및 resolver 작성

--- pages/api/resolvers/Query.js
+++ pages/api/resolvers/Query.js
@@ -1,5 +1,20 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
+function user(parent, args, context) {
+  return context.prisma.user.findUnique({
+    id: args.id,
+  });
+}
+
 function users(parent, args, context) {
   return context.prisma.user.findMany();
+}
+
+function response(parent, args, context) {
+  const where = args.id ? { id: args.id } : {};
+  return context.prisma.response.findMany({
+    where,
+  });
 }
 
 async function issues(parent, args, context) {
@@ -7,12 +22,44 @@ async function issues(parent, args, context) {
 
   const issues = await context.prisma.issue.findMany({
     where,
+    include: { post: true, response: true, issue_has_tag: true },
   });
 
   return issues;
 }
 
+async function issueHasTags(parent, args, context) {
+  const where = args.id ? { id: args.id } : {};
+  return await context.prisma.issue_has_tag.findMany({
+    where,
+    include: { tag: true },
+  });
+}
+
+async function posts(parent, args, context) {
+  const where = args.id ? { id: args.id } : {};
+  const posts = await context.prisma.post.findMany({
+    where,
+    include: { user: true, response: true },
+  });
+  return posts;
+}
+
+async function comments(parent, args, context) {
+  const where = args.id ? { id: args.id } : {};
+  const comments = await context.prisma.comment.findMany({
+    where,
+  });
+
+  return comments;
+}
+
 export default {
+  user,
   users,
   issues,
+  posts,
+  response,
+  comments,
+  issueHasTags,
 };
