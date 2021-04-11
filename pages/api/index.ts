c0142915fb8c04c2e15938b312f5e2ace1510b7a
Jeesoo Hong <jeesoo@dable.io>
2021-04-11 10:49:34 +0900

    type 명 복수에서 단수로 수정

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,11 +2,11 @@ import { PrismaClient } from '@prisma/client';
 import { ApolloServer } from 'apollo-server-micro';
 import Query from './resolvers/Query';
 import Mutation from './resolvers/Mutation';
-import Users from './resolvers/Users';
-import Opinions from './resolvers/Opinions';
-import Issues from './resolvers/Issues';
-import IssueHashTags from './resolvers/IssueHashTags';
-import OpinionComments from './resolvers/OpinionComments';
+import User from './resolvers/User';
+import Opinion from './resolvers/Opinions';
+import Issue from './resolvers/Issue';
+import IssueHashTag from './resolvers/IssueHashTag';
+import OpinionComment from './resolvers/OpinionComment';
 import fs from 'fs';
 import path from 'path';
 import { getUserId } from './utils';
@@ -16,11 +16,11 @@ const prisma = new PrismaClient();
 const resolvers = {
   Query,
   Mutation,
-  Users,
-  Opinions,
-  OpinionComments,
-  IssueHashTags,
-  Issues,
+  User,
+  Opinion,
+  OpinionComment,
+  IssueHashTag,
+  Issue,
 };
 
 const apolloServer = new ApolloServer({
