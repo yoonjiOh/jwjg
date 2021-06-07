ee17725db5602a24a456fd3dfc80a2de0c2b84a2
Juram Park <parkjuram@gmail.com>
2021-06-07 22:31:13 +0900

    send email for resetting password.

---
+++ pages/users/reset_password.tsx
@@ -0,0 +1,99 @@
+import { withAuthUser, AuthAction } from 'next-firebase-auth';
+import Layout from '../../components/Layout';
+import TextField from '@material-ui/core/TextField';
+import { useState } from 'react';
+import FormGroup from '@material-ui/core/FormGroup';
+import common_style from '../index.module.scss';
+import { useRouter } from 'next/router';
+import Button from '@material-ui/core/Button';
+import firebase from 'firebase/app';
+import Snackbar, { SnackbarOrigin } from '@material-ui/core/Snackbar';
+
+const headerTitle = '비밀번호 찾기';
+const auth = firebase.auth();
+
+function TermsOfService() {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [errorText, setErrorText] = useState('');
+  const [error, setError] = useState(false);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    // Email address validation.
+    if (!email.includes('@')) {
+      setError(true);
+      setErrorText('Email address format is invalid.');
+      return;
+    }
+
+    await auth
+      .sendPasswordResetEmail(email)
+      .then(async () => {
+        setOpenSnackbar(true);
+        await new Promise(r => setTimeout(r, 1000)).then(() => {
+          router.push('/');
+        });
+      })
+      .catch(function (error) {
+        setError(true);
+        setErrorText(error.code);
+      });
+  };
+
+  const headerInfo = {
+    headerType: 'editMode',
+    subTitle: headerTitle,
+  };
+
+  const handleChange = e => {
+    e.preventDefault();
+    setEmail(e.target.value);
+    setError(false);
+    setErrorText('');
+  };
+
+  const handleClose = () => {
+    setOpenSnackbar(false);
+  };
+
+  return (
+    <Layout title={headerTitle} headerInfo={headerInfo}>
+      <main className={common_style.main}>
+        <form onSubmit={handleSubmit}>
+          <FormGroup>
+            <TextField
+              id="email"
+              label="Email(i.e abc@def.com)"
+              value={email}
+              onChange={handleChange}
+              helperText={errorText}
+              variant="outlined"
+              error={error}
+            />
+            <Button type="submit" variant="outlined" color="primary">
+              Send pwd reset email
+            </Button>
+          </FormGroup>
+        </form>
+      </main>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        open={openSnackbar}
+        onClose={handleClose}
+        message="email sent!"
+      />
+    </Layout>
+  );
+}
+
+export default withAuthUser({
+  whenAuthed: AuthAction.REDIRECT_TO_APP,
+  whenUnauthedBeforeInit: AuthAction.SHOW_LOADER,
+  whenUnauthedAfterInit: AuthAction.RENDER,
+})(TermsOfService);
