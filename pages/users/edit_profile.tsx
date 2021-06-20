96d6e9bdf9af0ded42aecd6f0a3600db999ba547
Juram Park <parkjuram@gmail.com>
2021-06-20 10:42:01 +0900

    fixing sign-up bugs

--- pages/users/edit_profile.tsx
+++ pages/users/edit_profile.tsx
@@ -121,7 +121,7 @@ const EditProfile = (props: Props) => {
     await updateUserProfile({
       variables: {
         // @ts-ignore
-        id: parseInt(props.data.user.id),
+        id: parseInt(props.user.id),
         name: name,
         nickname: nickname,
         intro: intro,
@@ -130,7 +130,7 @@ const EditProfile = (props: Props) => {
     }).then(result => {
       if (result.data) {
         // @ts-ignore
-        router.push(`/users/mypage?userId=${props.data.user.id}`);
+        router.push('/users/mypage');
       } else {
         console.error('프로필 편집에 문제가 생겼습니다.');
       }
