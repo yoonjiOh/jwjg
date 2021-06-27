46fe3d11d2b86b54dde3c7adc00ea02f9ad4561d
Juram Park <parkjuram@gmail.com>
2021-06-27 23:24:51 +0900

    update apolloclient link uri.

--- apollo/apolloClient.ts
+++ apollo/apolloClient.ts
@@ -11,7 +11,7 @@ function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: createUploadLink({
-      uri: 'http://localhost:3000/api',
+      uri: 'http://localhost:8080/api',
       credentials: 'same-origin',
     }),
     cache: new InMemoryCache(),
