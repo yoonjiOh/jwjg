97d1929825895fd9bbf346242870bcfd24312bd9
Juram Park <parkjuram@gmail.com>
2021-06-28 14:58:42 +0900

    change link area for email singup.

--- pages/users/RegistrationWidget.tsx
+++ pages/users/RegistrationWidget.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 import Link from 'next/link';
 import s from './users.module.scss';
 import { startFacebookSigninFlow } from '../../lib/users';
 
 function RegistrationWidget() {
-    return (
-      <div className={s.registrationWidgetWrapper}>
-        <button className={s.btnDefault}>
-          <Link href={`/users/email_registration`}>이메일로 시작하기</Link>
-        </button>
-        <button className={s.btnFacebook} onClick={startFacebookSigninFlow}>
-          Facebook으로 시작하기
-        </button>
-      </div>
-    );
+  return (
+    <div className={s.registrationWidgetWrapper}>
+      <Link href={`/users/email_registration`}>
+        <button className={s.btnDefault}>이메일로 시작하기</button>
+      </Link>
+      <button className={s.btnFacebook} onClick={startFacebookSigninFlow}>
+        Facebook으로 시작하기
+      </button>
+    </div>
+  );
 }
 
 export default RegistrationWidget;
