5a4f9ac8c468109cb3b1883d288d9437739734e7
yoonjiO <ohyescoding@gmail.com>
2021-06-29 23:42:35 +0900

    이용약관 페이지 디자인 수정 && 리팩토링

--- pages/users/terms_of_service.tsx
+++ pages/users/terms_of_service.tsx
@@ -1,10 +1,8 @@
 import { withAuthUser, AuthAction } from 'next-firebase-auth';
 import Layout from '../../components/Layout';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from 'next/link';
 import { useState } from 'react';
 import common_style from '../index.module.scss';
+import u_style from './users.module.scss';
 import { useRouter } from 'next/router';
 
 const headerTitle = '약관 동의';
@@ -22,34 +20,53 @@ function TermsOfService() {
     headerType: 'editMode',
     subTitle: headerTitle,
     action: (
-      <button disabled={!agreed} onClick={handleSubmit}>
+      <button
+        className={`${u_style.headerBtn} ${!agreed && u_style.disabled}`}
+        disabled={!agreed}
+        onClick={handleSubmit}
+      >
         다음
       </button>
     ),
   };
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = () => {
     // if (event.target.checked) {
     //   console.log(['show temrs_of_service documentation']);
     // }
-    setAgreed(event.target.checked);
+
+    setAgreed(prevState => {
+      return !prevState;
+    });
   };
 
   return (
     <Layout title={headerTitle} headerInfo={headerInfo}>
       <main className={common_style.main}>
-        <div>약관에 동의해 주세요</div>
-        <FormControlLabel
-          control={<Checkbox checked={agreed} onChange={handleChange} color="primary" />}
-          label="이용 약관에 동의해요"
-        />
-        <a
-          target="_blank"
-          href="https://docs.google.com/document/d/1ezGQ7FzFbWPPsnzZjmZLWVFA0hI5N75wP0R1LQ17Dgg/edit?usp=sharing"
-          rel="noopener noreferrer"
-        >
-          더 보기
-        </a>
+        <div className={u_style.wrapper}>
+          <div className={u_style.bigHeader}>약관에 동의해 주세요</div>
+          <div className={u_style.agreeBread}>
+            <label>
+              <img
+                src={
+                  agreed
+                    ? 'https://jwjg-icons.s3.ap-northeast-2.amazonaws.com/ico_check_circle.png'
+                    : 'https://jwjg-icons.s3.ap-northeast-2.amazonaws.com/ico_check_circle--disable.png'
+                }
+              />
+              <input style={{ visibility: 'hidden' }} type="checkbox" onChange={handleChange} />
+              <span className={agreed && u_style.agreed}>이용 약관에 동의해요</span>
+            </label>
+            <a
+              target="_blank"
+              href="https://docs.google.com/document/d/1ezGQ7FzFbWPPsnzZjmZLWVFA0hI5N75wP0R1LQ17Dgg/edit?usp=sharing"
+              rel="noopener noreferrer"
+              style={{ textDecoration: 'underline' }}
+            >
+              더 보기
+            </a>
+          </div>
+        </div>
       </main>
     </Layout>
   );
