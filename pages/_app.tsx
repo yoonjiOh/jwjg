51fb93579227e2829bf09e7ee6843febf7abf933
Jeesoo Hong <jeesoo@dable.io>
2021-10-04 13:01:47 +0900

    add favicon and meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import '../styles/globals.scss';
 // import initAuth from '../utils/initAuth'; // the module you created above
 import { Provider } from 'next-auth/client';
@@ -11,11 +12,37 @@ import type { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
   return (
-    <ApolloProvider client={apolloClient}>
-      <Provider session={pageProps.session}>
-        <Component {...pageProps} />
-      </Provider>
-    </ApolloProvider>
+    <>
+      <Head>
+        <title>좌우지간</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon-precomposed" href="/fav_152.png" />
+        <link rel="icon" href="/fav_16.png" sizes="16x16" />
+        <link rel="icon" href="/fav_32.png" sizes="32x32" />
+        <link rel="icon" href="/fav_96.png" sizes="96x96" />
+        <link rel="icon" href="/fav_152.png" sizes="152x152" />
+        <link rel="icon" href="/fav_167.png" sizes="167x167" />
+        <link rel="icon" href="/fav_180.png" sizes="180x180" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://jwjg-icons.s3.ap-northeast-2.amazonaws.com/img_meta.png"
+        />
+        <meta
+          property="og:description"
+          content="우리 이제 화해해요! 서로를 이해하는 이슈 SNS, 좌우지간"
+        />
+        <meta property="og:site_name" content="좌우지간" />
+        <meta property="og:locale" content="ko_KR" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
+      <ApolloProvider client={apolloClient}>
+        <Provider session={pageProps.session}>
+          <Component {...pageProps} />
+        </Provider>
+      </ApolloProvider>
+    </>
   );
 }
 
